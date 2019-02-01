0ac3e177a8761248615ad6634794265683ff15b2
tushar-c <tushar.chat192@gmail.com>
2019-02-01 13:02:15 +0530

    added suites

--- ManagOrgan/app/src/main/java/com/example/managorgan/MainActivity.java
+++ ManagOrgan/app/src/main/java/com/example/managorgan/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.managorgan;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
@@ -14,16 +18,24 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView greet_text;
+    Button name_button;
+    EditText name_input;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DatabaseReference firebase_db = FirebaseDatabase.getInstance().getReference();
-        firebase_db.child("niggz").setValue("foo");
+        name_button = findViewById(R.id.nameButton);
+        greet_text = findViewById(R.id.MainTextView);
+        name_input = findViewById(R.id.editText);
+
+        final DatabaseReference firebase_db = FirebaseDatabase.getInstance().getReference();
+        firebase_db.child("niggz").setValue("Welcome");
         firebase_db.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                TextView greet_text = findViewById(R.id.MainTextView);
+
                 greet_text.setText(dataSnapshot.child("niggz").getValue().toString());
             }
 
@@ -32,6 +44,27 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    name_button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            final String paad = firebase_db.push().getKey();
+            firebase_db.child("user").child(paad).child("name").setValue(name_input.getText().toString());
+
+            firebase_db.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    greet_text.setText(dataSnapshot.child("user").child(paad).child("name").getValue().toString());
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+            Toast.makeText(MainActivity.this, "Data added and Text updated Successfully", Toast.LENGTH_SHORT).show();
+        }
+    });
 
     }
 }
